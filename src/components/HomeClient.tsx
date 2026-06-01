36b892dffef63d5e16e7d87b284fe36f7ea22dcc
sayan-acharyya <acharyyas735@gmail.com>
2026-06-02 01:44:20 +0530

    that for today

---
+++ src/components/HomeClient.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react'
+import { motion } from "motion/react"
+
+const Homeclient = () => {
+    const handleLogin = () => {
+        window.location.href = "/api/auth/login"
+    }
+
+    return (
+        <div className='min-h-screen bg-linear-to-br from-white to-zinc-50 text-zinc-900 overflow-x-hidden'>
+            <motion.div
+                initial={{ y: -50 }}
+                animate={{ y: 0 }}
+                transition={{ duration: 0.5 }}
+                className='fixed  top-0 left-0 w-full z-50 bg-white/70 backdrop-blur-xl border-b border-zinc-200'
+            >
+                <div className='max-w-7xl mx-auto px-6 h-16 flex items-center justify-between'>
+                    <div className='text-lg font-semibold tracking-tight'>
+                        Support<span className='text-zinc-400 ml-0.5'>AI</span>
+                    </div>
+                    <button
+                    onClick={handleLogin}
+                        className='px-5 py-2 rounded-full bg-black text-white text-sm font-medium hover:bg-zinc-800 
+                    transition disabled:opacity-60 flex items-center gap-2'
+                    >
+                        Login
+                    </button>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
+
+export default Homeclient
