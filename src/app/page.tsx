4e398496925ac803f72edd966367d14d77bc7f9a
sayan-acharyya <acharyyas735@gmail.com>
2026-06-02 11:19:21 +0530

    workinh on authjentication in scalejkit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Homeclient from "@/components/HomeClient";
+import { getSession } from "@/lib/getSession";
 
  
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession()
+  console.log(session);
+  //1:12:40
   return (
     <>
         <Homeclient/>
