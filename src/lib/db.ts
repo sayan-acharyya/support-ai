c1c959ba0c8658401ac87152010775262b85e948
sayan-acharyya <acharyyas735@gmail.com>
2026-06-04 12:31:45 +0530

    mongo db connection done

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,32 @@
+import { connect } from "mongoose";
 
 const mongo_url = process.env.MONGODB_URL
-if(!mongo_url){
+if (!mongo_url) {
     console.log("mongodb url not found");
-    
+
 }
-//2:26:20
+
+let cache = global.mongoose
+if (!cache) {
+    cache = global.mongoose = { conn: null, promise: null }
+}
+
+const connectDb = async () => {
+    if (cache.conn) {
+        return cache.conn
+    }
+
+    if (!cache.promise) {
+        cache.promise = connect(mongo_url!).then((c)=>c.connection)
+    }
+
+    try {
+        cache.conn = await cache.promise
+    } catch (error) {
+        console.log(error);
+
+    }
+    return cache.conn
+}
+
+export default connectDb;
