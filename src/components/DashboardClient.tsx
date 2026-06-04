38219dcfbb970a1b65f0366506615682be068c2d
sayan-acharyya <acharyyas735@gmail.com>
2026-06-04 21:55:52 +0530

    api working efficrently

--- src/components/DashboardClient.tsx
+++ src/components/DashboardClient.tsx
@@ -1,14 +1,66 @@
 
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { motion } from "motion/react"
 import { useRouter } from 'next/navigation'
+import axios from 'axios'
+import { Loader2 } from 'lucide-react'
+import toast from 'react-hot-toast'
 
 const DashboardClient = ({ ownerId }: { ownerId: string }) => {
     const router = useRouter();
     const [businessName, setBusinessName] = useState("");
     const [supportEmail, setSupportEmail] = useState("");
     const [knowledge, setKnowledge] = useState("");
+    const [loading, setLoading] = useState(false);
+
+
+    const handleSettings = async () => {
+
+        if (!businessName || !supportEmail || !knowledge) {
+            toast.error("Please fill all fields")
+            return;
+        }
+
+        setLoading(true)
+
+        try {
+            const result = await axios.post(
+                "/api/settings",
+                { ownerId, businessName, supportEmail, knowledge }
+            )
+
+            toast.success("Chatbot settings updated")
+
+            setBusinessName("");
+            setSupportEmail("");
+            setKnowledge("");
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Failed to update chatbot settings")
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if (ownerId) {
+            const handleGetDetails = async () => {
+                try {
+                    const result = await axios.post("/api/settings/get", { ownerId });
+                    setBusinessName(result.data.businessName)
+                    setSupportEmail(result.data.supportEmail)
+                    setKnowledge(result.data.knowledge);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+            handleGetDetails()
+        }
+        
+    }, [ownerId])
 
     return (
         <div className='min-h-screen bg-zinc-50 text-zinc-900'>
@@ -46,14 +98,14 @@ const DashboardClient = ({ ownerId }: { ownerId: string }) => {
                             <input
                                 type="text"
                                 value={businessName}
-                                onChange={(e)=>setBusinessName(e.target.value)}
+                                onChange={(e) => setBusinessName(e.target.value)}
                                 placeholder='Business Name'
                                 className='w-full rounded-xl border border-zinc-300 px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black/80'
                             />
                             <input
                                 type="text"
                                 value={supportEmail}
-                                onChange={(e)=>setSupportEmail(e.target.value)}
+                                onChange={(e) => setSupportEmail(e.target.value)}
                                 placeholder='Support email'
                                 className='w-full rounded-xl border border-zinc-300 px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-black/80'
 
@@ -66,8 +118,8 @@ const DashboardClient = ({ ownerId }: { ownerId: string }) => {
                         <p className='text-sm text-zinc-500 mb-4'>Add FAQs, policies, delivery info, refunds, etc.</p>
                         <div className='space-y-4'>
                             <textarea
-                            value={knowledge}
-                            onChange={(e)=>setKnowledge(e.target.value)}
+                                value={knowledge}
+                                onChange={(e) => setKnowledge(e.target.value)}
                                 placeholder={`• Refund Policy: 7 Days Return Available
 • Delivery Time: 3–5 Working Days
 • Cash on Delivery Available
@@ -80,11 +132,13 @@ const DashboardClient = ({ ownerId }: { ownerId: string }) => {
 
                     <div className='flex items-center gap-5'>
                         <motion.button
+                            onClick={handleSettings}
+                            disabled={loading}
                             whileHover={{ scale: 1.03 }}
                             whileTap={{ scale: 0.97 }}
-                            className='px-7 py-3 rounded-xl bg-black text-white text-sm font-medium hover:bg-zinc-900 transition disabled:opacity-60'
+                            className='px-7 py-3 rounded-xl bg-black text-white text-sm font-medium hover:bg-zinc-900 transition disabled:opacity-60  '
                         >
-                            Save
+                            {loading ? <Loader2 size={15} className='animate-spin' /> : " Save"}
                         </motion.button>
                     </div>
 
